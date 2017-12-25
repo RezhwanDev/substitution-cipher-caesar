6e12576bd571794945a73f42b06e6eb6402720c3
Zhwan <rezhwansidiq@gmail.com>
2017-12-25 10:27:51 -0800

    Upload all The Files

---
+++ Caesar Cipher/Caesar Done/Caesar.java
@@ -0,0 +1,62 @@
+/**
+ * @author Rezhwan Sidiq
+ * 21/Nov/2017
+ * MidtermExam Study (-_-
+ * 20:18
+ */
+
+    public class Caesar {
+
+    char alpha[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+    public void enc(String s,int key){
+
+      String  ss=s.toUpperCase();
+      char [] message =ss.toCharArray();
+      char [] result =new char[message.length];
+      int count ;
+
+      for (int i = 0; i < message.length; i++) {
+            for (int j = 0; j < alpha.length; j++) {
+                if (message[i] == alpha[j]) {
+                    count=(j+key)%26;
+                    result[i]=alpha[count];
+                }
+            }
+        }
+        String Sresult = new String (result);
+        System.out.println("Final Result of encryption = "+Sresult);
+    }
+
+    public void dec(String s,int key){
+
+    String  ss=s.toUpperCase();
+    char [] message =ss.toCharArray();
+    char [] result =new char[message.length];
+    int count ;
+
+    for (int i = 0; i < message.length; i++) {
+        for (int j = 0; j < alpha.length; j++) {
+            if (message[i] == alpha[j]) {
+                    count=j-key;
+                    if (count < 0) {
+                    count=count+26;
+                     result[i]=alpha[count];
+                } else result[i]=alpha[count];
+            }
+        }
+    }
+        String Sresult = new String (result);
+        System.out.println("Final Result of Decryption = "+Sresult);
+    }
+
+    public static void main(String[] args) {
+        Caesar cob = new Caesar();
+        cob.enc("Rezhwan",5);
+        cob.dec("WJEMBFS",5);
+    }
+}
+/*  OutPut
+Final Result of encryption = WJEMBFS
+Final Result of Decryption = REZHWAN
+*/
